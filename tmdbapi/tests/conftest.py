81511664ff07d718c33c9385dffbe63fb25aaa28
Patrick Liu <32678320+patrick-csliu@users.noreply.github.com>
2023-10-06 05:18:04 +0800

    fixed release

--- tmdbapi/tests/conftest.py
+++ tmdbapi/tests/conftest.py
@@ -1,5 +1,6 @@
 import shutil
 from pathlib import Path
+import pytest
 
 # @pytest.fixture(autouse=True)
 # def run_before_and_after_tests(tmpdir):
@@ -12,15 +13,40 @@ from pathlib import Path
 #     # clean up after tests
 #     shutil.rmtree("tmdbapi/tests/temp", ignore_errors=True)
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--cred",
+        action="store",
+        help="Specify the path to the credential file",
+        type=Path,
+    )
 
-def pytest_configure():
+
+# @pytest.fixture
+# def credential_path(request) -> str:
+#     path = request.config.getoption("--cred")
+#     # pyth = pytest.Config().getoption("--cred")
+#     if path is None:
+#         return Path("test.credential").absolute()
+#     else:
+#         return path.absolute()
+
+
+def pytest_configure(config):
     """
     Allows plugins and conftest files to perform initial configuration.
     This hook is called for every plugin and initial conftest
     file after command line options have been parsed.
     """
+    path = config.getoption("--cred")
+    if path is None:
+        cred_path =  Path("test.credential").absolute()
+    else:
+        cred_path =  path.absolute()
+    print(cred_path)
+    # print("!!!!!!!!!!!!!!!!!!", type(credential_path), credential_path)
     Path("tmdbapi/tests/temp").mkdir(parents=True, exist_ok=True)
-    shutil.copyfile("test.credential", "tmdbapi/tests/temp/test.credential")
+    shutil.copyfile(cred_path, "tmdbapi/tests/temp/test.credential")
 
 
 def pytest_unconfigure():
